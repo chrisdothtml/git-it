7e9991e97d910ce8e320b789f168a9dac9650e0e
Chris Deacy <chrisdothtml@gmail.com>
2018-06-07 10:04:44 -0500

    adding persistence for window position and size

--- node/index.js
+++ node/index.js
@@ -1,26 +1,35 @@
 const path = require('path')
+const windowStateKeeper = require('electron-window-state')
 const { app, BrowserWindow, ipcMain } = require('electron')
 const { getReposFromDir } = require('./git.js')
 
 app.on('ready', () => {
-  const win = new BrowserWindow({
+  const windowState = windowStateKeeper({
+    defaultWidth: 400,
+    defaultHeight: 625
+  })
+  const window = new BrowserWindow({
     acceptFirstMouse: true,
     backgroundColor: '#fff',
     fullscreen: false,
-    height: 625,
+    height: windowState.height,
+    icon: path.join(__dirname, '../icons/icon.icns'),
     maximizable: false,
     resizable: false,
-    width: 400,
-    titleBarStyle: 'hidden',
     title: 'GitIt',
-    icon: path.join(__dirname, '../icons/icon.icns'),
+    titleBarStyle: 'hidden',
+    width: windowState.width,
+    x: windowState.x,
+    y: windowState.y,
   })
 
   if (process.env.NODE_ENV === 'production') {
-    win.loadFile('dist/splash.html')
+    window.loadFile('dist/splash.html')
   } else {
-    win.loadURL(`http://localhost:${process.env.PORT || 8080}/splash.html`)
+    window.loadURL(`http://localhost:${process.env.PORT || 8080}/splash.html`)
   }
+
+  windowState.manage(window)
 })
 
 ipcMain.on('fetch-repos', (event) => {
