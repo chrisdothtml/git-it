3fb21888a7e31603aee1701ab979663652f13929
Chris Deacy <chrisdothtml@gmail.com>
2018-06-07 09:13:25 -0500

    renaming splash files; moving splash icp event handlers to splash component

---
+++ browser/screens/splash/Splash.jsx
@@ -0,0 +1,59 @@
+import Octicon from '../../common/Octicon.jsx'
+import React from 'react'
+import { ipcRenderer } from 'electron'
+import './Splash.css'
+
+function Repo (repo, i) {
+  return (
+    <li key={ i } className="repo">
+      <button className="clickbox">
+        <span className="repo-name">{ repo.name }</span>
+        <div className="repo-branch">
+          <Octicon name="git-branch" />
+          <span className="branch-label">{ repo.branch }</span>
+        </div>
+      </button>
+    </li>
+  )
+}
+
+export default class Splash extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      loading: true,
+      repos: []
+    }
+  }
+
+  componentDidMount () {
+    ipcRenderer.send('fetch-repos')
+    ipcRenderer.on('receive-repos', (event, repos) => {
+      this.setState({
+        loading: false,
+        repos
+      })
+    })
+  }
+
+  render () {
+    const hasRepos = !this.state.loading && this.state.repos.length
+    const repoList = hasRepos ? this.state.repos.map(Repo) : ''
+
+    return (
+      <div className="splash-page">
+        <div className="toolbar">
+          <div className="ghost-bg"></div>
+          <div className="content">
+            <button><Octicon name="plus" /></button>
+            <button><Octicon name="gear" /></button>
+          </div>
+        </div>
+        <ul className="repos">
+          { repoList }
+        </ul>
+      </div>
+    )
+  }
+}
--- browser/screens/splash/index.js
+++ browser/screens/splash/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Splash from './index.jsx'
-import { ipcRenderer } from 'electron'
+import Splash from './Splash.jsx'
 import { hot } from 'react-hot-loader'
 
-ipcRenderer.send('fetch-repos')
-ipcRenderer.on('receive-repos', (event, repos) => {
-  ReactDOM.render(
-    React.createElement(hot(module)(Splash), { repos }),
-    document.getElementById('root')
-  )
-})
+ReactDOM.render(
+  React.createElement(hot(module)(Splash)),
+  document.getElementById('root')
+)
