beb8b0b50799d60cd6beadc1487061239031e36b
Chris Deacy <chrisdothtml@gmail.com>
2018-06-08 10:12:31 -0500

    misc tweaks

--- node/index.js
+++ node/index.js
@@ -2,11 +2,22 @@ const path = require('path')
 const windowStateKeeper = require('electron-window-state')
 const { app, BrowserWindow, ipcMain } = require('electron')
 const { getReposFromDir } = require('./git.js')
+const { buildQuery } = require('./utils.js')
+
+function loadScreen (window, screen, data = {}) {
+  const query = buildQuery(data)
+
+  if (process.env.NODE_ENV === 'production') {
+    window.loadFile(`dist/${screen}.html?${query}`)
+  } else {
+    window.loadURL(`http://localhost:${process.env.PORT || 8080}/${screen}.html?${query}`)
+  }
+}
 
 app.on('ready', () => {
   const windowState = windowStateKeeper({
+    defaultHeight: 625,
     defaultWidth: 400,
-    defaultHeight: 625
   })
   const window = new BrowserWindow({
     acceptFirstMouse: true,
@@ -23,12 +34,7 @@ app.on('ready', () => {
     y: windowState.y,
   })
 
-  if (process.env.NODE_ENV === 'production') {
-    window.loadFile('dist/splash.html')
-  } else {
-    window.loadURL(`http://localhost:${process.env.PORT || 8080}/splash.html`)
-  }
-
+  loadScreen(window, 'splash')
   windowState.manage(window)
 })
 
@@ -37,9 +43,7 @@ ipcMain.on('fetch-repos', (event) => {
   const reposPath = '/Users/chris/projects'
 
   getReposFromDir(reposPath)
-    .then(repos => {
-      event.sender.send('receive-repos', repos)
-    })
+    .then(repos => event.sender.send('receive-repos', repos))
     .catch(e => console.error(e))
 })
 
--- node/utils.js
+++ node/utils.js
@@ -1,5 +1,11 @@
 const fs = require('pfs')
 
+exports.buildQuery = data => {
+  return Object.keys(data)
+  .map(key => `${key}=${data[key]}`)
+  .join('&')
+}
+
 exports.pathExists = filepath => {
   return fs.access(filepath)
     .then(() => true)
